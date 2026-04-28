b3ce22c30f0854f3a10940144979ae7e3b747e6e
PaholaClarisse <paholamartinelagos@gmail.com>
2026-04-28 11:40:13 +0200

    wip: frontend jinja2 en cours

--- app/main.py
+++ app/main.py
@@ -3,14 +3,37 @@ from app.routes import auth, formulaires
 from app.database import settings
 from bson import ObjectId
 from fastapi.encoders import ENCODERS_BY_TYPE
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.staticfiles import StaticFiles
 
 ENCODERS_BY_TYPE[ObjectId] = str  # Convertir ObjectId en string lors de la sérialisation
 
 app = FastAPI(title="API de Collecte de Données", description="Une API pour la collecte de données avec authentification JWT", version="1.0.0")
 
-@app.get("/")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+'''@app.get("/")
 def root():
-    return {"message": "Bienvenue sur l'API de Collecte de Données!"}
+    return {"message": "Bienvenue sur l'API de Collecte de Données!"}'''
     
 app.include_router(auth.router)
 app.include_router(formulaires.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html",{"request": request})
+
+@app.get("/f/{formulaire_id}", response_class=HTMLResponse)
+async def formulaire_public(request: Request, formulaire_id: str):
+    return templates.TemplateResponse("formulaire_public.html",{"request": request, "formulaire_id": formulaire_id})
+
+@app.get("/formulaires/{formulaire_id}/reponses", response_class=HTMLResponse)
+async def voir_reponses(request: Request, formulaire_id: str):
+    return templates.TemplateResponse("reponses.html", {"request": request, "formulaire_id": formulaire_id})
