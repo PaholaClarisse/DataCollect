3f5a41b1fb22d89ae8c1f51723017e3c96a7d515
LMPAI@ <paholamartinelagos@gmail.com>
2026-04-30 01:18:02 +0100

    Refactor MONGO_URI to use environment variable

--- app/configure.py
+++ app/configure.py
@@ -5,11 +5,8 @@ import os
 load_dotenv()
 
 class Settings:
-    PORT = os.getenv("PORT", 27017)
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
-    MONGO_URI = f"mongodb://{DB_USER}:{DB_PASSWORD}@localhost:{PORT}/{DB_NAME}?authSource=admin"
+    MONGO_URI = os.getenv("MONGO_URI")
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
